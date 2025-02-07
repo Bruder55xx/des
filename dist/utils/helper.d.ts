b30040a79da3878aff644b09c5178e6aa597516e
Bruder <181657834+Bruder55xx@users.noreply.github.com>
2025-02-07 16:57:47 +0300

    Update helper.d.ts

--- dist/utils/helper.d.ts
+++ dist/utils/helper.d.ts
@@ -4,5 +4,3 @@ export declare function delay(ms: number): Promise<unknown>;
 export declare function saveToFile(filename: string, data: string): Promise<void>;
 export declare function readFile(pathFile: string): Promise<string[]>;
 export declare const newAgent: (proxy?: string | null) => SocksProxyAgent | HttpsProxyAgent<string> | null;
-import keep_alive from './keep_alive.js';
-keep_alive();
